3948e6ff49fb3bf5936d354c2f92acfbec07b5c4
howardyoo <howardyoo@gmail.com>
2025-09-24 10:56:15 -0500

    fixes

--- frontend/src/telemetry.ts
+++ frontend/src/telemetry.ts
@@ -17,7 +17,7 @@ export function initTelemetry() {
     sdk = new HoneycombWebSDK({
       // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
       debug: true, // Set to false for production environment.
-      apiKey: 'hcaik_01jk983xknkpchnpg3cg1s419844yyj8ywm7dgj7kjc9a4a7qth9hx7z59', // Replace with your Honeycomb Ingest API Key.
+      apiKey: '<HONEYCOMB_API_KEY>', // Replace with your Honeycomb Ingest API Key.
       serviceName: 'docassistant-frontend', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
       instrumentations: [getWebAutoInstrumentations({
         // Loads custom configuration for xml-http-request instrumentation.
