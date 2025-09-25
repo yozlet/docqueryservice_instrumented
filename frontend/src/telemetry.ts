f611e03cf3da80f90569bc9ccac26c9ad6666797
howardyoo <howardyoo@gmail.com>
2025-09-25 11:41:32 -0500

    minor changes and fixes

--- frontend/src/telemetry.ts
+++ frontend/src/telemetry.ts
@@ -1,5 +1,3 @@
-
-
 import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 
@@ -12,13 +10,16 @@ const configDefaults = {
 
 var sdk: HoneycombWebSDK | null = null;
 
-const endpoint = process.env.REACT_APP_BROWSER_OTEL_EXPORTER_OTLP_ENDPOINT;
+const apiKey = process.env.REACT_APP_HONEYCOMB_API_KEY;
+const endpoint = process.env.REACT_APP_OTEL_COLLECTOR_URL;
 
 export function initTelemetry() {
   if (sdk == null) {
     sdk = new HoneycombWebSDK({
       endpoint: endpoint,
-      debug: true, // Set to false for production environment.
+      // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
+      debug: false, // Set to false for production environment.
+      apiKey: apiKey, // Replace with your Honeycomb Ingest API Key.
       serviceName: 'docassistant-frontend', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
       instrumentations: [getWebAutoInstrumentations({
         // Loads custom configuration for xml-http-request instrumentation.
