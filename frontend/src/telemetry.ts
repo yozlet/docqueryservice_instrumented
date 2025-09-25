9b3c98da54f7359f18ce15a2140f7de4fa410f00
Pierre Tessier <pierre@pierretessier.com>
2025-09-25 00:00:05 -0400

    docker compose services

--- frontend/src/telemetry.ts
+++ frontend/src/telemetry.ts
@@ -12,16 +12,13 @@ const configDefaults = {
 
 var sdk: HoneycombWebSDK | null = null;
 
-const apiKey = process.env.REACT_APP_HONEYCOMB_API_KEY;
-const endpoint = process.env.REACT_APP_HONEYCOMB_ENDPOINT;
+const endpoint = process.env.REACT_APP_BROWSER_OTEL_EXPORTER_OTLP_ENDPOINT;
 
 export function initTelemetry() {
   if (sdk == null) {
     sdk = new HoneycombWebSDK({
       endpoint: endpoint,
-      // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
       debug: true, // Set to false for production environment.
-      apiKey: apiKey, // Replace with your Honeycomb Ingest API Key.
       serviceName: 'docassistant-frontend', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
       instrumentations: [getWebAutoInstrumentations({
         // Loads custom configuration for xml-http-request instrumentation.
